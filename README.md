779d4d8e7c92125cbd04bd51b4c4772f392c6a60
Computa Hacker Boy <thiagofigcosta@hotmail.com>
2021-11-12 13:48:42 -0300

    added original code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # disk-and-network-benchmark-python
+
+This code was based on the following repositories, all credits to them:
+>- https://github.com/thodnev/MonkeyTest
+>- https://github.com/amjadsde/Speed-Test
---
+++ network-test/server.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import sys, time
+from socket import *
+import threading
+
+BUFSIZE = 1024000
+port = input("Port: ")
+def server():
+    s = socket(AF_INET, SOCK_STREAM)
+    s.bind(('0.0.0.0', int(port)))
+    s.listen(1)
+    print ('Server ready...')
+    while 1:
+        conn, (host, remoteport) = s.accept()
+        while 1:
+            data = conn.recv(BUFSIZE)
+            if not data:
+                break
+            del data
+        #conn.send('OK\n')
+        conn.close()
+        print ('Done with', host, 'port', remoteport)
+server()
